578543bf60eafcbafb9f746602c058f3c9b16dbb
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-27 23:04:42 -0400

    adding the dashboard and homeRoute render profiles, and a starter code for the main page

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,83 @@
+const router = require("exress").Router();
+const withAuth = require("../utils/auth");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", withAuth, async (req, res) => {
+  // withAuth will send you back to the utils/auth and test if there is a req.session.userId
+  // if there is NOT it will redirect you back to the login page
+  // if there IS (next in utils/auth) it will move to the route handling below
+
+  try {
+    const postStuff = await Post.findAll({
+      where: { userId: req.session.userId },
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: 
+            {
+              model: User,
+              attributes: ["username"],
+            },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        }
+      ]
+    });
+
+    const posts = postStuff.map((post) => post.get({ plain: true }));
+    res.render("dashboard", { posts, loggedIn: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+    res.redirect("login");
+  }
+});
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postSingle = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    });
+
+    const post = postSingle.get({ plain: true });
+    if (!post) {
+      res.status(404).end();
+    }
+    res.render("edit-post", {post, loggedIn: true});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
